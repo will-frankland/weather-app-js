249bab249c25324e739747ad8b409101fb6ebf07
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-08-03 11:57:02 -0400

    add geolocation tracker to grab coordinates from browser

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 window.addEventListener('load', ()=> {
-  
+  let long;
+  let lat;
+
+  if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(position => {
+      console.log(position);
+    })
+  } else {
+    h1.textContent = "browser not compatible"
+  }
 });
