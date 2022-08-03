c7ef3263274a097e1d77329b618b1d550807254f
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-08-03 12:23:49 -0400

    integrate openWeatherMap api into app.js

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-window.addEventListener('load', ()=> {
+window.addEventListener('load', () => {
   let long;
   let lat;
 
-  if (navigator.geolocation){
+  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
-      console.log(position);
-    })
-  } else {
-    h1.textContent = "browser not compatible"
+      // console.log(position);
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid={45d14f33fabe16763a7406c21cb0a722}`;
+
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+        })
+
+    });
+
   }
 });
